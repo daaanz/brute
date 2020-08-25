94d5c6847edc901fce902ffa8ad84792d2c0cda6
esfnbr <esfortnitebr@gmail.com>
2020-08-25 17:30:55 +0200

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,14 +1,11 @@
 import random
 import requests
 import string
-import tweepy
 import io
 from PIL import Image
 from io import BytesIO
-
-auth = tweepy.OAuthHandler("wZdsf58Ka2jciAifsd5NDg2y1", "FwbZcX6g9vGbpwLyNejLIHdY9KybOhUdEBHVTvXn6YQpa1MpRY")
-auth.set_access_token("1285954782545563649-gM0P4hinHv47aYX9EATIgidTOzxS89", "9vBzMVJOh21YbylaR01cwQZs3T1zhRCvKScFHFMsbEoci")
-api = tweepy.API(auth)
+import os
+from os import environ
 
 url_base = "https://cdn2.unrealengine.com/en-14br-teaser-day4-motd-1920x1080-1920x1080-"
 jpg_ext = ".jpg"
@@ -17,9 +14,9 @@ url_live = 'https://cdn2.unrealengine.com/en-14br-teaser-day1-motd-1920x1080-192
 def download_img(url, file_name):
     r = requests.get(url)
     im = Image.open(BytesIO(r.content))
-    im.save(number_current + '.jpg')
+    im.save(ImageID + '.jpg')
 
-number_start = 839200000
+number_start = 845070000
 
 while 1:
     try:
@@ -29,14 +26,18 @@ while 1:
         status = url_status.status_code
         print(url)
         print(status)
+        if status == 200:
+            try:
+                print('Imagen detectada.')
+                print(number_current)
+                ImageID = str(number_current)
+                file = (ImageID + '.jpg')
+                download_img(url, file)
+                print('Se ha guardado correctamente.')
+            except:
+                print('Error al descargar.')
         number_start = number_current
-        if status == '200':
-            print('Imagen detectada.')
-            file = (number_current + '.jpg')
-            download_img(url, file)
-            print('Se ha guardado correctamente.')
-            api.update_with_media(number_current + '.jpg', 'text')
-            print('Se ha publicado en Twitter')
+
     except:
         print('Err.')
 
